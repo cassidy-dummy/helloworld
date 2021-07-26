e82c11df50ff57c155a9d8070a6f357573f51a6b
cassidy <cassidy@foghornconsulting.com>
2021-07-26 16:17:06 -0400

    simplify to use nginx for taskdef

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-gunicorn -c /edx/app/xqueue/xqueue/docker_gunicorn_configuration.py --bind=0.0.0.0:8040 --workers 2 --max-requests=1000 xqueue.wsgi:application
+# gunicorn -c /edx/app/xqueue/xqueue/docker_gunicorn_configuration.py --bind=0.0.0.0:8040 --workers 2 --max-requests=1000 xqueue.wsgi:application
+nginx -g 'daemon off;'
