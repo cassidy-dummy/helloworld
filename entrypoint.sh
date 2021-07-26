2380f9d1cf70efb4965f32620af2681dddc18e0a
cassidy <cassidy@foghornconsulting.com>
2021-07-26 18:57:24 -0400

    use xqueue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# gunicorn -c /edx/app/xqueue/xqueue/docker_gunicorn_configuration.py --bind=0.0.0.0:8040 --workers 2 --max-requests=1000 xqueue.wsgi:application
-nginx -g 'daemon off;'
+gunicorn -c /edx/app/xqueue/xqueue/docker_gunicorn_configuration.py --bind=0.0.0.0:8040 --workers 2 --max-requests=1000 xqueue.wsgi:application
+# nginx -g 'daemon off;'
