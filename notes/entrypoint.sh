6c52869282da01a0a437564b340c530909ab3216
cassidy <cassidy@foghornconsulting.com>
2021-08-05 15:36:52 -0400

    ALLOWED_HOSTS to entrypoint in notes

--- notes/entrypoint.sh
+++ notes/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo '{}' > /edx/etc/edx_notes_api.yml
+cat <<EOF > /edx/etc/edx_notes_api.yml
+ALLOWED_HOSTS: '*'
+EOF
+
 gunicorn --workers=2 --name notes -c /edx/app/notes/notesserver/docker_gunicorn_configuration.py --log-file - --max-requests=1000 notesserver.wsgi:application
